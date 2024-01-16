07c628e20e2468388b8a2691be82445926d999b0
hwiju yeom <yeom456@gmail.com>
2024-01-16 16:10:40 +0900

    드럼 커밋완

---
+++ bj/30890.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	static StringBuilder sb = new StringBuilder();
+	static int x;
+	static int y;
+
+	public static void main(String[] args) throws IOException {
+
+		String[] split = br.readLine().split(" ");
+
+		x = Integer.parseInt(split[0]);
+		y = Integer.parseInt(split[1]);
+
+		int[] arr = new int[x * y + 1];
+
+		for (int i = 1; i < arr.length; i++) {
+
+			if (i % x == 0 && i % y == 0) {
+				sb.append(3);
+			} else if (i % x == 0) {
+				sb.append(2);
+			} else if (i % y == 0) {
+				sb.append(1);
+			}
+		}
+
+		System.out.println(sb.toString());
+
+	}
+
+}
